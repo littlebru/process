4a377f09a378bcb6c7e7970d4290dedb18fb54ed
Bruna Gomes <heathjared2@gmail.com>
2020-08-15 15:45:13 -0300

    Add process in Java language

---
+++ Java/Process.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class OSProcess{
+	public static void main(String[] args) throws IOException{
+		if(args.length !=1){
+			System.err.println("Usage: java OSProcess <command>");
+			System.exit(0);
+		}
+
+//args[0] é o commando
+ProcessBuilder pb = new ProcessBuilder(args[0]);
+Process proc = pb.start();
+
+// Obtem a entrada do sistema
+InputStream is = proc.getInputStream();
+InputStreamReader isr = new InputStreamReader(is);
+
+BufferedReader br = new BufferedReader(isr);
+
+// Lendo o retorno do comando
+String line;while((line = br.readLine())!= null)System.out.println(line);
+br.close();
+}
+}
