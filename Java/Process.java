c519a3ed9bff00ef137d3585d58ba17a9e047a58
Bruna Gomes <brunaclegomes@hotmail.com>
2020-08-15 15:47:16 -0300

    update document

--- Java/Process.java
+++ Java/Process.java
@@ -1,24 +1,30 @@
 import java.io.*;
 
-public class OSProcess{
-	public static void main(String[] args) throws IOException{
+public class Process{
+	public static void main(String[] args) throws IOException
+	{
 		if(args.length !=1){
 			System.err.println("Usage: java OSProcess <command>");
 			System.exit(0);
 		}
 
-//args[0] é o commando
-ProcessBuilder pb = new ProcessBuilder(args[0]);
-Process proc = pb.start();
+		//args[0] é o commando
+		ProcessBuilder pb = new ProcessBuilder(args[0]);
+		Process proc = pb.start();
 
-// Obtem a entrada do sistema
-InputStream is = proc.getInputStream();
-InputStreamReader isr = new InputStreamReader(is);
+		// Obtem a entrada do sistema
+		InputStream is = proc.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
 
-BufferedReader br = new BufferedReader(isr);
+		BufferedReader br = new BufferedReader(isr);
 
-// Lendo o retorno do comando
-String line;while((line = br.readLine())!= null)System.out.println(line);
-br.close();
-}
+		// Lendo o retorno do comando
+		String line;
+		
+		while((line = br.readLine())!= null)
+			System.out.println(line);
+		
+		br.close();
+		
+	}
 }
