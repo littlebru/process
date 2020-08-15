dd8f22e8db9aa0334edb3ff5b6e2c61b446bf5c8
Bruna Gomes <brunaclegomes@hotmail.com>
2020-08-15 15:38:01 -0300

    formatting document

--- C/process.c
+++ C/process.c
@@ -12,19 +12,19 @@ int main(){
 
 	if(pid <0){
 		fprintf(stderr,"Fork failed");
-		exit(-1);//Saida forçada do sistema}
+		exit(-1);//Saida forçada do sistema
+	}
 	
 	// Condição para processo filho
 	else if (pid ==0){
-	execlp("/bin/ls","ls", NULL);
+		execlp("/bin/ls","ls", NULL);
 	}
 	
 	//Condição para processo pai
-		else {
-	
-	//O pai só executa após a finalização do filho
-	wait(NULL);
-	printf("Child Complete");
-	exit(0);
+	else {
+		//O pai só executa após a finalização do filho
+		wait(NULL);
+		printf("Child Complete");
+		exit(0);
 	}
 }
